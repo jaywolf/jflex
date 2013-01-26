dc7b830a99019fafd4c78636371977434ac5cab1
J Wolf <wolf.jay@gmail.com>
2013-01-25 18:45:16 -0800

    initial commit of jFlex SASSematic ResponsiGrid Drupal base theme

---
+++ template.php
@@ -0,0 +1,232 @@
+<?php
+
+/**
+ * Override or insert variables into the html template.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ */
+function jflex_preprocess_html(&$vars) {
+  // add mobile metas
+  // viewport 
+  $meta_viewport = array(
+    '#type' => 'html_tag',
+    '#tag' => 'meta',
+    '#weight' => '5',
+    '#attributes' => array(
+      'name' => 'viewport',
+      'content' => 'width=device-width, initial-scale=1.0'
+    )
+  );
+  drupal_add_html_head($meta_viewport, 'viewport');
+  // handheldfriendly
+  $meta_handheld = array(
+    '#type' => 'html_tag',
+    '#tag' => 'meta',
+    '#weight' => '10',
+    '#attributes' => array(
+      'name' => 'HandheldFriendly',
+      'content' => 'True'
+    )
+  );
+  drupal_add_html_head($meta_handheld, 'HandheldFriendly');
+  $meta_mobileopt = array(
+    '#type' => 'html_tag',
+    '#tag' => 'meta',
+    '#weight' => '15',
+    '#attributes' => array(
+      'name' => 'MobileOptimized',
+      'content' => 'width'
+    )
+  );
+  drupal_add_html_head($meta_mobileopt, 'MobileOptimized');
+  
+  // give <body> tag a unique class depending on PATHs
+  $path_alias = strtolower(drupal_clean_css_identifier(drupal_get_path_alias($_GET['q'])));
+  if ($path_alias == 'node') {
+    $vars['classes_array'][] = '';
+  }
+  else {
+    $vars['classes_array'][] = 'path-'. $path_alias;
+  }
+  
+  // Add to the array of body classes
+  // layout classes
+  $vars['classes_array'][] = 'layout-'. (!empty($vars['page']['sidebar_first']) ? 'first-main' : 'main') . (!empty($vars['page']['sidebar_second']) ? '-second' : '');
+  // headers classes
+  if (!empty($vars['page']['header_first']) || !empty($vars['page']['header_second']) || !empty($vars['page']['header_third'])) {
+    $header_regions = 'header';
+    $header_regions .= (!empty($vars['page']['header_first'])) ? '-first' : '';
+    $header_regions .= (!empty($vars['page']['header_second'])) ? '-second' : '';
+    $header_regions .= (!empty($vars['page']['header_third'])) ? '-third' : '';
+    $vars['classes_array'][] = $header_regions;
+  }
+  // preface classes
+  if (!empty($vars['page']['preface_first']) || !empty($vars['page']['preface_second']) || !empty($vars['page']['preface_third'])) {
+    $preface_regions = 'preface';
+    $preface_regions .= (!empty($vars['page']['preface_first'])) ? '-first' : '';
+    $preface_regions .= (!empty($vars['page']['preface_second'])) ? '-second' : '';
+    $preface_regions .= (!empty($vars['page']['preface_third'])) ? '-third' : '';
+    $vars['classes_array'][] = $preface_regions;
+  }
+  // postscripts classes
+  if (!empty($vars['page']['postscript_first']) || !empty($vars['page']['postscript_second']) || !empty($vars['page']['postscript_third'])) {
+    $postscript_regions = 'postscript';
+    $postscript_regions .= (!empty($vars['page']['postscript_first'])) ? '-first' : '';
+    $postscript_regions .= (!empty($vars['page']['postscript_second'])) ? '-second' : '';
+    $postscript_regions .= (!empty($vars['page']['postscript_third'])) ? '-third' : '';
+    $vars['classes_array'][] = $postscript_regions;
+  }
+  // footers classes
+  if (!empty($vars['page']['footer_first']) || !empty($vars['page']['footer_second']) || !empty($vars['page']['footer_third'])) {
+    $footer_regions = 'footers';
+    $footer_regions .= (!empty($vars['page']['footer_first'])) ? '-first' : '';
+    $footer_regions .= (!empty($vars['page']['footer_second'])) ? '-second' : '';
+    $footer_regions .= (!empty($vars['page']['footer_third'])) ? '-third' : '';
+    $vars['classes_array'][] = $footer_regions;
+  }
+  // Panels classes
+  $vars['classes_array'][] = (module_exists('panels') && (panels_get_current_page_display())) ? 'panels' : '';
+  if (module_exists('panels') && (panels_get_current_page_display())) {
+    $panels_display = panels_get_current_page_display();
+    $vars['classes_array'][] = 'panels-'. strtolower(preg_replace('/[^a-zA-Z0-9-]+/', '-', $panels_display->layout));
+  }
+  
+  $vars['classes_array'] = array_filter($vars['classes_array']);
+  
+  // IE stylesheets
+  //drupal_add_css(path_to_theme() . '/css/ie8-fixes.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'IE 8', '!IE' => FALSE), 'preprocess' => FALSE));
+}
+
+
+// implement hook_html_head_alter() to alter meta.
+function jflex_html_head_alter(&$head_elements) {
+  $head_elements['system_meta_content_type']['#attributes'] = array(
+    'charset' => 'utf-8',
+  );
+}
+
+
+/**
+ * Override or insert variables into the page template.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ */
+function jflex_preprocess_page(&$vars) {
+  // Add preface, postscript, & footers classes with number of active sub-regions
+  $region_list = array(
+    'prefaces' => array('preface_first', 'preface_second', 'preface_third'), 
+    'postscripts' => array('postscript_first', 'postscript_second', 'postscript_third'),
+    'footers' => array('footer_first', 'footer_second', 'footer_third')
+  );
+  foreach ($region_list as $sub_region_key => $sub_region_list) {
+    $active_regions = array();
+    foreach ($sub_region_list as $region_item) {
+      if (!empty($vars['page'][$region_item])) {
+        $active_regions[] = $vars['page'][$region_item];
+      }
+    }
+    $vars[$sub_region_key] = $sub_region_key .'-'. strval(count($active_regions));
+  }
+  
+  // Render menu tree from main-menu
+  $menu_tree = menu_tree('main-menu');
+  $vars['main_menu_tree'] = render($menu_tree);
+}
+
+
+/**
+ * Override or insert variables into the node templates.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ */
+function jflex_preprocess_node(&$vars) {
+  global $theme, $user;
+  $vars['classes_array'][] = $vars['zebra'];
+  if ($vars['view_mode'] == 'full') {
+    $vars['classes_array'][] = 'node-full';
+  }
+  
+  // Utilize the time tag w/ pubdate attribute for node submit date
+  $vars['datetime'] = format_date($vars['created'], 'custom', 'c');
+  if (variable_get('node_submitted_' . $vars['node']->type, TRUE)) {
+    $vars['submitted'] = t('Submitted by !username on !datetime',
+      array(
+        '!username' => $vars['name'],
+        '!datetime' => '<time datetime="' . $vars['datetime'] . '" pubdate="pubdate">' . $vars['date'] . '</time>',
+      )
+    );
+  }
+  else {
+    $vars['submitted'] = '';
+  }
+  
+  // Add node-type-page template suggestion
+  if ($vars['page']) {
+    $vars['theme_hook_suggestions'][] = 'node__'. $vars['node']->type .'_page';
+    $vars['theme_hook_suggestions'][] = 'node__'. $vars['node']->type .'-'. $vars['node']->nid .'_page';
+  }
+  else {
+    $vars['theme_hook_suggestions'][] = 'node__'. $vars['node']->type .'_teaser';
+    $vars['theme_hook_suggestions'][] = 'node__'. $vars['node']->nid;
+  }
+}
+
+
+/**
+ * Preprocess variables for region.tpl.php
+ *
+ * Prepare the values passed to the theme_region function to be passed into a
+ * pluggable template engine. Uses the region name to generate a template file
+ * suggestions. If none are found, the default region.tpl.php is used.
+ *
+ * @see region.tpl.php
+ */
+function jflex_preprocess_region(&$vars) {
+  // Sidebar region template suggestion.
+  if (strpos($vars['region'], 'sidebar_') === 0) {
+    $vars['theme_hook_suggestions'][] = 'region__sidebar';
+    $vars['theme_hook_suggestions'][] = 'region__' . $vars['region'];
+  }
+}
+
+
+/**
+ * Override or insert variables into the block templates.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ */
+function jflex_preprocess_block(&$vars) {
+  $block = $vars['block'];
+  // First/last block position
+  $vars['position'] = ($vars['block_id'] == 1) ? 'first' : '';
+  if ($vars['block_id'] == count(block_list($block->region))) {
+    $vars['position'] = ($vars['position']) ? '' : 'last';
+  }
+  
+  // Add template suggestion for menu blocks
+  $nav_blocks = array('navigation', 'main-menu', 'management', 'user-menu');
+  if (in_array($vars['block']->delta, $nav_blocks)) {
+    $vars['theme_hook_suggestions'][] = 'block__menu';
+  }
+}
+
+
+/**
+ * Override or insert variables into the comment templates.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ */
+function jflex_preprocess_comment(&$vars) {
+  // Add odd/even classes to comments classes_array 
+  static $comment_odd = TRUE;
+  $vars['classes_array'][] = $comment_odd ? 'odd' : 'even';
+  $comment_odd = !$comment_odd;
+  
+  // Utilize the time tag w/ pubdate attribute for comment submit date
+  $vars['datetime'] = format_date($vars['comment']->created, 'custom', 'c');
+}
---
+++ templates/page/page.tpl.php
@@ -0,0 +1,320 @@
+<?php
+/**
+ * @file
+ * Theme implementation to display a single Drupal page.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/garland.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or
+ *   prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $main_menu (array): An array containing the Main menu links for the
+ *   site, if they have been configured.
+ * - $secondary_menu (array): An array containing the Secondary menu links for
+ *   the site, if they have been configured.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.tpl.php):
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ * - $messages: HTML for status and error messages. Should be displayed
+ *   prominently.
+ * - $tabs (array): Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node).
+ * - $action_links (array): Actions local to the page, such as 'Add menu' on the
+ *   menu administration interface.
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $node: The node object, if there is an automatically-loaded node
+ *   associated with the page, and the node ID is the second argument
+ *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - $page['help']: Dynamic help text, mostly for admin pages.
+ * - $page['highlight']: Items for the highlighted content region.
+ * - $page['content']: The main content of the current page.
+ * - $page['sidebar_first']: Items for the first sidebar.
+ * - $page['sidebar_second']: Items for the second sidebar.
+ * - $page['header']: Items for the header region.
+ * - $page['footer']: Items for the footer region.
+ * - $page['bottom']: Items to appear at the bottom of the page below the footer.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see template_process()
+ *
+ * 
+ */
+?>
+<div id="page">
+  <?php if ($page['header_top']): ?>
+    <div id="header-top" class="container">
+      <div class="row">
+        <div class="column">
+          <div class="inner">
+            <?php print render($page['header_top']); ?>
+          </div><!-- /inner -->
+        </div><!-- /column -->
+      </div><!-- /row -->
+    </div><!-- /container -->
+  <?php endif; ?>
+  
+  <?php $header_first_region = $logo || $site_name || $site_slogan || $page['header_first']; ?>
+  <?php if ($header_first_region || $header_second || $header_third): ?>
+    <header id="header" class="container">
+      <div class="row">
+        <?php if ($header_first_region): ?>
+          <div id="header-first" class="column">
+            <div class="inner">
+              <?php if ($logo): ?>
+              <div id="logo">
+                <a href="<?php print check_url($front_page) ?>" title="<?php print t('Home') ?>">
+                  <img src="<?php print $logo ?>" alt="<?php print t('Home') ?>" />
+                </a>
+              </div>
+              <?php endif; ?>
+              
+              <?php if ($site_name || $site_slogan): ?>
+              <hgroup>
+                <?php if ($site_name): ?>
+                  <h1 id="site-name">
+                    <a href="<?php print check_url($front_page) ?>" title="<?php print t('Home'); ?>">
+                      <?php print $site_name; ?>
+                    </a>
+                  </h1>
+                <?php endif; ?>
+                <?php if ($site_slogan): ?>
+                  <h2 id="slogan">
+                    <?php print $site_slogan; ?>
+                  </h2>
+                <?php endif; ?>
+              </hgroup>
+              <?php endif; ?>
+              <?php if ($page['header_first']): ?>
+                <?php print render($page['header_first']); ?>
+              <?php endif; ?>
+            </div><!-- /column inner -->
+          </div><!-- /header-first column -->
+        <?php endif; ?>
+
+        <?php if ($page['header_second']): ?>
+          <div id="header-second" class="column">
+            <div class="inner">
+              <?php print render($page['header_second']); ?>
+            </div><!-- /column inner -->
+          </div><!-- /header-second column -->
+        <?php endif; ?>  
+        
+        <?php if ($page['header_third']): ?>
+          <div id="header-third" class="column">
+            <div class="inner">
+              <?php print render($page['header_third']); ?>
+            </div><!-- /column inner -->
+          </div><!-- /header-third column -->
+        <?php endif; ?> 
+      </div><!-- header row -->
+    </header><!-- /header container -->
+  <?php endif; ?>
+  
+  <?php if ($main_menu || $secondary_menu || $page['header_bottom']): ?>
+    <div id="header-bottom" class="container">
+      <div class="row">
+        <div class="column">
+          <div class="inner">
+            <?php if ($main_menu): ?>
+              <nav id="main-menu">
+                <h2 class="element-invisible">Main menu</h2>
+                <?php print $main_menu_tree; ?>
+              </nav><!-- /main-menu -->
+            <?php endif; ?>
+              
+            <?php if ($secondary_menu): ?>
+              <nav id="secondary-menu">
+                <h2 class="element-invisible">Secondary menu</h2>
+                <?php print theme('links__system_secondary_menu', array(
+                  'links' => $secondary_menu,
+                )); ?>
+              </nav><!-- /secondary-menu -->
+            <?php endif; ?>
+                  
+            <?php if ($page['header_bottom']): ?>
+              <?php print render($page['header_bottom']); ?>
+            <?php endif; ?>
+          </div><!-- /inner --> 
+        </div><!-- /column -->
+      </div><!-- /header-bottom row -->
+    </div><!-- /header-bottom container --> 
+  <?php endif; ?>
+  
+  <?php if ($page['preface_first'] || $page['preface_second'] || $page['preface_third']): ?>
+    <div id="prefaces" class="container <?php print $prefaces; ?>">
+      <h2 class="element-invisible">Preface content</h2>
+      <div class="row">
+        <?php if ($page['preface_first']): ?>
+          <div id="preface-first" class="column">
+            <div class="inner">
+              <?php print render($page['preface_first']); ?>
+            </div><!-- /column inner -->
+          </div><!-- /preface-first column -->
+        <?php endif; ?>
+        <?php if ($page['preface_second']): ?>
+          <div id="preface-second"class="column">
+            <div class="inner">
+              <?php print render($page['preface_second']); ?>
+            </div><!-- /column inner -->
+          </div><!-- /preface-second column -->
+        <?php endif; ?>
+        <?php if ($page['preface_third']): ?>
+          <div id="preface-third" class="column">
+            <div class="inner">
+              <?php print render($page['preface_third']); ?>
+            </div><!-- /column inner -->
+          </div><!-- /preface-third column -->
+        <?php endif; ?>
+      </div><!-- /prefaces row -->
+    </div><!-- /prefaces container -->
+  <?php endif; ?>
+  
+  <?php $content_top_region = $page['help'] || $messages || $page['highlight']; ?>
+  <?php $content_region = $tabs || $title || $page['content']; ?>
+  <?php if ($content_top_region || $content_region || $page['sidebar_first'] || $page['sidebar_second']): ?>
+    <div id="main" class="container">
+      <div class="row">
+        <?php if ($page['sidebar_first']): ?>
+          <aside id="sidebar-first" class="sidebar column">
+            <div class="inner">
+              <h2 class="element-invisible">Sidebar content</h2>
+              <?php print render($page['sidebar_first']); ?>
+            </div><!-- /column inner -->
+          </aside><!-- /sidebar-first column -->
+        <?php endif; ?>
+        
+        <div id="main-content" class="column">
+          <div class="inner">
+            <?php if ($content_top_region): ?>
+              <div id="content-top">
+                <?php if ($page['help']): ?>
+                  <?php print render($page['help']); ?>
+                <?php endif; ?>
+                <?php if ($messages): ?>
+                  <?php print $messages; ?>
+                <?php endif; ?>
+                <?php if ($page['highlight']): ?>
+                  <div id="highlighted">
+                    <?php print render($page['highlight']); ?>
+                  </div>
+                <?php endif; ?>
+              </div><!-- /content-top -->
+            <?php endif; ?>
+            <?php if ($content_region): ?>
+              <?php if (!empty($tabs['#primary']) || $title): ?>
+                <header>
+                  <?php if (!empty($tabs['#primary'])): ?>
+                    <div id="content-tabs" class="clearfix">
+                      <?php print render($tabs); ?>
+                    </div>
+                  <?php endif; ?>
+                  <?php if ($action_links): ?>
+                    <ul class="action-links"><?php print render($action_links); ?></ul>
+                  <?php endif; ?>
+                </header>
+              <?php endif; ?>
+              <?php if ($page['content']): ?>
+                <?php print render($page['content']); ?>
+              <?php endif; ?>
+            <?php endif; ?>
+          </div><!-- /inner -->
+        </div><!-- /main-content column -->
+        
+        <?php if ($page['sidebar_second']): ?>
+          <aside id="sidebar-second" class="sidebar column">
+            <div class="inner">
+              <h2 class="element-invisible">Sidebar content</h2>
+              <?php print render($page['sidebar_second']); ?>
+            </div><!-- /column inner -->
+          </aside><!-- /sidebar-second column -->
+        <?php endif; ?>
+      </div><!-- /main-content row -->
+    </div><!-- /main-content container -->
+  <?php endif; ?>
+  
+  <?php if ($page['postscript_first'] || $page['postscript_second'] || $page['postscript_third']): ?>
+    <div id="postscripts" class="container <?php print $postscripts; ?>">
+      <h2 class="element-invisible">Postscript content</h2>
+      <div class="row">
+        <?php if ($page['postscript_first']): ?>
+          <div id="postscript-first" class="column fourcol first">
+            <div class="inner">
+              <?php print render($page['postscript_first']); ?>
+            </div><!-- /column inner -->
+          </div><!-- /postscript-first column -->
+        <?php endif; ?>
+        <?php if ($page['postscript_second']): ?>
+          <div id="postscript-second"class="column">
+            <div class="inner">
+              <?php print render($page['postscript_second']); ?>
+            </div><!-- /column inner -->
+          </div><!-- /postscript-second column -->
+        <?php endif; ?>
+        <?php if ($page['postscript_third']): ?>
+          <div id="postscript-third" class="column">
+            <div class="inner">
+              <?php print render($page['postscript_third']); ?>
+            </div><!-- /column inner -->
+          </div><!-- /postscript-third column -->
+        <?php endif; ?>
+      </div><!-- /postscript row -->
+    </div><!-- /postscript container -->
+  <?php endif; ?>
+
+  <?php if ($page['footer_first'] || $page['footer_second'] || $page['footer_third']): ?>
+    <div id="footers" class="container <?php print $footers; ?>">
+      <h2 class="element-invisible">Footer content</h2>
+      <div class="row">
+        <?php if ($page['footer_first']): ?>
+          <div id="footer-first" class="column">
+            <div class="inner">
+              <?php print render($page['footer_first']); ?>
+            </div><!-- /column inner -->
+          </div><!-- /footer-first column -->
+        <?php endif; ?>
+        <?php if ($page['footer_second']): ?>
+          <div id="footer-second"class="column">
+            <div class="inner">
+              <?php print render($page['footer_second']); ?>
+            </div><!-- /column inner -->
+          </div><!-- /footer-second column -->
+        <?php endif; ?>
+        <?php if ($page['footer_third']): ?>
+          <div id="footer-third" class="column">
+            <div class="inner">
+              <?php print render($page['footer_third']); ?>
+            </div><!-- /column inner -->
+          </div><!-- /footer-third column -->
+        <?php endif; ?>
+      </div><!-- /footer row -->
+    </div><!-- /footer container -->
+  <?php endif; ?>
+</div><!-- /page -->
